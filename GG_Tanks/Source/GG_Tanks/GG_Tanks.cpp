035b6a75275c7e0395881a98cac11e98178a6ac0
DESKTOP-IUOLOIM\steli <thequaryy@gmail.com>
2017-08-13 13:39:53 +0300

    Project Created In Unreal

---
+++ GG_Tanks/Source/GG_Tanks/GG_Tanks.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GG_Tanks.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GG_Tanks, "GG_Tanks" );
